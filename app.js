94034c91c3e1d3ab70439d74c61cd1bb173786db
Benjamin Fisher <neohell@hotmail.com>
2012-08-08 01:53:03 -0700

    Demonstration of directory middleware

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/dir', routes.dir);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
